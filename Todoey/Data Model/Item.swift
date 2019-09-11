270bb224364c00722e5123f6ba8a3c49cf58ffba
Andre Suhartanto <andresuhartanto28@gmail.com>
2019-09-11 16:12:11 +0800

    Add MVC pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Andre Suhartanto on 11/09/2019.
+//  Copyright © 2019 Andre Suhartanto. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
